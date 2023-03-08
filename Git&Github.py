463a2b0e9883cf12a2457f6ff9fb4257624a9f3d
baran.karakurt <hbarankarakurt@gmail.com>
2023-03-08 13:37:49 +0300

    deneme degisiklik

---
+++ Git&Github.py
@@ -0,0 +1,13 @@
+
+
+#git init projeye sahip ol, destek ol, göz kulak ol demesidir
+#kırmızı working area
+#git add dosya ismi ile lokale taşıyoruz
+#artık rengi yeşile döndü
+#git add . ile tüm working spaceler eklenir
+#kaydetmek için git commit -m "mesajı yaz / bilgisayarın hafızasına kaydoluyor artık böylece
+
+
+
+
+
