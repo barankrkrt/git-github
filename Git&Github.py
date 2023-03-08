32b31c35659907f916ef0854fce8b10c7e58fd0e
baran.karakurt <hbarankarakurt@gmail.com>
2023-03-08 14:54:53 +0300

    modifiye

--- Git&Github.py
+++ Git&Github.py
@@ -6,6 +6,19 @@
 #artık rengi yeşile döndü
 #git add . ile tüm working spaceler eklenir
 #kaydetmek için git commit -m "mesajı yaz / bilgisayarın hafızasına kaydoluyor artık böylece
+#git rm direkt siler work space'i böylece takip bırakılır
+
+#github'tan açtığımız repository'i bağlaması adına git remote add origin dedik
+#kontrol için de git remote -v'i kullandık / böylece push ve puıll yapabileceğimiz bir remote var mı görmüş olduk
+#lokalimizde olan dosyaları hala pushlamadık
+#bunu yapmak için de git push origin master ile master'a pusluyoruz. (cloud'a gönderiliyor)
+#lokalden bir şey aktarılıyorsa push işlemi yapılır
+#remote'tan değişiklik çekmek isteniyorsa pull yapılır
+#yeni oluşturdupumuz branch ana branch'in kopyası oluyor daima
+#branch oluşturmak ve geçmek için git checkout -b branch ismi
+
+
+
 
 
 
